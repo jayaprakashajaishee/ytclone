2ac9c7a0e8fce0b53417c7a3ca050124d6044746
jayaprakashajaishee <jayaprakashajaishee@gmail.com>
2023-12-19 18:00:12 +0530

    comments section added

--- src/types/types.ts
+++ src/types/types.ts
@@ -6,3 +6,88 @@ export interface VideoData {
   prevPageToken?: string;
   pageInfo: { totalResults: number; resultsPerPage: number };
 }
+
+export interface RootComment {
+  kind: string;
+  etag: string;
+  nextPageToken: string;
+  pageInfo: PageInfo;
+  items: Item[];
+}
+
+interface Item {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: CommentSnippet2;
+  replies?: Replies;
+}
+
+interface Replies {
+  comments: Comment[];
+}
+
+interface Comment {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: Snippet3;
+}
+
+interface Snippet3 {
+  channelId: string;
+  videoId: string;
+  textDisplay: string;
+  textOriginal: string;
+  parentId: string;
+  authorDisplayName: string;
+  authorProfileImageUrl: string;
+  authorChannelUrl: string;
+  authorChannelId: AuthorChannelId;
+  canRate: boolean;
+  viewerRating: string;
+  likeCount: number;
+  publishedAt: string;
+  updatedAt: string;
+}
+
+interface CommentSnippet2 {
+  channelId: string;
+  videoId: string;
+  topLevelComment: TopLevelComment;
+  canReply: boolean;
+  totalReplyCount: number;
+  isPublic: boolean;
+}
+
+interface TopLevelComment {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: CommentSnippet;
+}
+
+interface CommentSnippet {
+  channelId: string;
+  videoId: string;
+  textDisplay: string;
+  textOriginal: string;
+  authorDisplayName: string;
+  authorProfileImageUrl: string;
+  authorChannelUrl: string;
+  authorChannelId: AuthorChannelId;
+  canRate: boolean;
+  viewerRating: string;
+  likeCount: number;
+  publishedAt: string;
+  updatedAt: string;
+}
+
+interface AuthorChannelId {
+  value: string;
+}
+
+interface PageInfo {
+  totalResults: number;
+  resultsPerPage: number;
+}
